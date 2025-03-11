c868c2f42fe9d6ea1966737cfa703e1a032b9e20
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-03-11 17:24:09 +0400

    update

--- scripts/eval_knn_whitebox_attack.sh
+++ scripts/eval_knn_whitebox_attack.sh
@@ -3,17 +3,19 @@
 
 # Default values for arguments
 EXP_NUM=${1:-1}
-EPSILON=${2:-8}
-STEP_SIZE=${3:-10}
-BATCH_SIZE=${4:-32}
+MODEL_NAME=${2:-"resnet50"}
+EPSILON=${3:-8}
+STEP_SIZE=${4:-10}
+BATCH_SIZE=${5:-32}
+
 
 
 # Run evaluation scripts
 
-bash scripts/adv_knn.sh $EXP_NUM false
-bash scripts/adv_knn.sh $EXP_NUM true $EPSILON $STEP_SIZE $BATCH_SIZE "bimr_knn"
-bash scripts/adv_knn.sh $EXP_NUM true $EPSILON $STEP_SIZE $BATCH_SIZE "pgd_knn"
-bash scripts/adv_knn.sh $EXP_NUM true $EPSILON $STEP_SIZE $BATCH_SIZE "mifgsmr_knn"
+bash scripts/adv_knn.sh $EXP_NUM $MODEL_NAME false
+bash scripts/adv_knn.sh $EXP_NUM $MODEL_NAME true $EPSILON $STEP_SIZE $BATCH_SIZE "bimr_knn"
+bash scripts/adv_knn.sh $EXP_NUM $MODEL_NAME true $EPSILON $STEP_SIZE $BATCH_SIZE "pgd_knn"
+bash scripts/adv_knn.sh $EXP_NUM $MODEL_NAME true $EPSILON $STEP_SIZE $BATCH_SIZE "mifgsmr_knn"
 
 
 echo "All evaluation scripts executed successfully!"
